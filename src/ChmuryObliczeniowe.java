3291b67c7e1a0cd6593c23b9633994c84d6c3b6c
Michal <michal.wasilewski94@gmail.com>
2020-03-29 12:27:46 +0200

    Added project name in main class

--- src/ChmuryObliczeniowe.java
+++ src/ChmuryObliczeniowe.java
@@ -1,5 +1,7 @@
 public class ChmuryObliczeniowe {
     public static void main(String[] args) {
         System.out.printf("Java version: %s", System.getProperty("java.version"));
+        System.out.println();
+        System.out.println("Nazwa projektu: chmury_obliczeniowe_cw1");
     }
 }
